8bba795813b91746424a1afe4d9ab67b51891b89
Matt Jackson <matt@jacksonsix.com>
2013-03-05 17:30:48 -0800

    Finished Chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,6 +14,19 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+        describe "after submission" do
+          before { click_button submit }
+
+          it { should have_selector('title', text: 'Sign up') }
+          it { should have_content('error') }
+          it { should have_content('Name can\'t be blank') }
+          it { should have_content('Email can\'t be blank') }
+          it { should have_content('Email is invalid') }
+          it { should have_content('Password can\'t be blank') }
+          it { should have_content('Password is too short') }
+          it { should have_content('Password confirmation can\'t be blank') }
+        end
     end
 
     describe "with valid information" do
@@ -24,11 +37,18 @@ describe "User pages" do
         fill_in "Confirmation",  with: "foobar"
       end
 
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
       it "should create user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
-
 
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title',  text: 'Sign up') }
