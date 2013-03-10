067ba4cddb36c96ba8b088b4f88ba62ce1d14ab9
Matt Jackson <matt@jacksonsix.com>
2013-03-10 12:29:29 -0700

    finally fixed the user_controller so that tests pass

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,8 +17,21 @@ module SessionsHelper
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def udpate
+    @user = User.find(params[:id])
+  end
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,6 +4,14 @@ describe "Authentication" do
 
   subject { page }
 
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_selector('h1',     text: 'Sign in') }
+    it { should have_selector('title',    text: 'Sign in') }
+
+  end
+
   describe "signin" do
 
     before { visit signin_path }
@@ -32,6 +40,42 @@ describe "Authentication" do
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
+      end
+    end
+  end
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+    end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user }
+
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_selector('title', text: full_title('Edit user')) }
+      end
+
+      describe "submitting a PUT request to the Users#update action" do
+        before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
       end
     end
   end
